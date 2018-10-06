94757fed761ac4f5b5c9174e8354504542a8bad1
Unknown <asaenzcordero@gmail.com>
2018-10-06 02:02:07 -0500

    color navbar

--- app/init.js
+++ app/init.js
@@ -7,18 +7,9 @@ $(function(){
 
 	});
 	
-	$('.dropdown-trigger').dropdown({
-		inDuration: 500,
-		outDuration: 1000,
-		contrainWidth: false,
-          hover: true,
-        	
-        	belowOrigin: true,
-          alignment: 'right',
-          stopPropagation: false
-        });
-	
-       $('.collapsible').collapsible();
+    $('.collapsible').collapsible();
+
+    $('.parallax').parallax();
   });
 
 
