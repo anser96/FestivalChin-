b6bb97e44de402b3e33072ac304467a89c13ded3
Unknown <asaenzcordero@gmail.com>
2018-10-06 01:14:40 -0500

    cambio de dropdwon a collapsible sidenav

--- app/init.js
+++ app/init.js
@@ -1,16 +1,24 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav(),
+$(function(){
+    $('.sidenav').sidenav();
+
     $(".nav").dropdown({
 		hover: true,
 		coverTrigger: false
 
-	}),
+	});
+	
 	$('.dropdown-trigger').dropdown({
+		inDuration: 500,
+		outDuration: 1000,
+		contrainWidth: false,
           hover: true,
-          coverTrigger: false,
-          alignment: 'right'
+        	
+        	belowOrigin: true,
+          alignment: 'right',
+          stopPropagation: false
         });
-      
+	
+       $('.collapsible').collapsible();
   });
 
 
