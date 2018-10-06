be86bc60d0104a759388ea6b63d393c1930f0e89
Unknown <asaenzcordero@gmail.com>
2018-10-05 23:11:49 -0500

    Dropdown Navbar

---
+++ app/init.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+$(document).ready(function() {
+	$(".dropdown-trigger").dropdown({
+		hover: true,
+		coverTrigger: false
+
+	});
+});
