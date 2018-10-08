8a38c5e73aeb3082cdc3116a687af856547d8164
Luis Jaramillo Negrete <ljaramillonegrete@gmail.com>
2018-10-08 03:38:33 -0500

    Cambio de estructura

--- app/init.js
+++ app/init.js
@@ -6,12 +6,19 @@ $(function(){
 		coverTrigger: false
 
 	});
-	
+
     $('.collapsible').collapsible();
 
     $('.parallax').parallax();
+
+    $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
   });
 
-
-
-
+  function autoplay() {
+    $('.carousel').carousel('next');
+    setTimeout(autoplay, 5000);
+  }
+  setTimeout(autoplay, 5000);
+  });
